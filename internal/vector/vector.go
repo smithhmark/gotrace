55825c72628ea99df70464ef2cfa7726f5b5efd8
Mark Smith <smith.h.mark@gmail.com>
2025-11-14 13:25:18 -0700

    adding more vector functions

--- internal/vector/vector.go
+++ internal/vector/vector.go
@@ -1,15 +1,60 @@
 package vector
 
+import (
+	"math"
+)
+
 type SVector3 [3]float32
+
+func almostEqual(a, b, epsilon float32) bool {
+	dif := float64(a - b)
+	return math.Abs(dif) < float64(epsilon)
+}
+
+func (l SVector3) Almost(r SVector3) bool {
+	ep := float32(0.000001)
+	return almostEqual(l[0], r[0], ep) && almostEqual(l[1], r[1], ep) && almostEqual(l[2], r[2], ep)
+}
 
 func (l SVector3) Add(r SVector3) SVector3 {
 	var res = SVector3{l[0] + r[0], l[1] + r[1], l[2] + r[2]}
-	//res := SVector3{l[0] + r[0], l[1] + r[1], l[2] + r[2]}
 	return res
 }
 
 func (l SVector3) Sub(r SVector3) SVector3 {
 	var res = SVector3{l[0] - r[0], l[1] - r[1], l[2] - r[2]}
-	//res := SVector3{l[0] + r[0], l[1] + r[1], l[2] + r[2]}
 	return res
+}
+
+func (l SVector3) Scale(k float32) SVector3 {
+	var res = SVector3{l[0] * k, l[1] * k, l[2] * k}
+	return res
+}
+
+func (l SVector3) Dot(r SVector3) float32 {
+	var res float32
+	res = l[0]*r[0] + l[1]*r[1] + l[2]*r[2]
+	return res
+}
+
+func (l SVector3) Mag() float32 {
+	var res float64
+	res = float64(l[0])*float64(l[0]) + float64(l[1])*float64(l[1]) + float64(l[2])*float64(l[2])
+	return float32(math.Sqrt(res))
+}
+
+func (l SVector3) Cross(r SVector3) SVector3 {
+	x := l[1]*r[2] - l[2]*r[1]
+	y := l[0]*r[2] - l[2]*r[0]
+	z := l[0]*r[1] - l[1]*r[0]
+	return SVector3{x, -1 * y, z}
+}
+
+func (l SVector3) Norm() SVector3 {
+	mag := l.Mag()
+	if mag == 0.0 {
+		return SVector3{0, 0, 0}
+	}
+	mag = 1 / mag
+	return l.Scale(mag)
 }
--- internal/vector/vector_test.go
+++ internal/vector/vector_test.go
@@ -87,3 +87,175 @@ func TestSub(t *testing.T) {
 		}
 	}
 }
+
+func TestScale(t *testing.T) {
+	tests := []struct {
+		left   SVector3
+		right  float32
+		result SVector3
+	}{
+		{
+			left:   SVector3{0, 0, 0},
+			right:  100.0,
+			result: SVector3{0, 0, 0},
+		},
+		{
+			left:   SVector3{1, 1, 1},
+			right:  1.0,
+			result: SVector3{1, 1, 1},
+		},
+		{
+			left:   SVector3{1, 2, 3},
+			right:  10.0,
+			result: SVector3{10, 20, 30},
+		},
+	}
+	for _, test := range tests {
+		l := test.left
+		r := test.right
+		rcvd := l.Scale(r)
+		if rcvd != test.result {
+			t.Errorf("%v.Scale(%v) != %v, got: %v", l, r, test.result, rcvd)
+
+		}
+	}
+}
+
+func TestMag(t *testing.T) {
+	tests := []struct {
+		left   SVector3
+		result float32
+	}{
+		{
+			left:   SVector3{0, 0, 0},
+			result: 0.0,
+		},
+		{
+			left:   SVector3{3, 4, 0},
+			result: 5.0,
+		},
+		{
+			left:   SVector3{0, 5, 12},
+			result: 13.0,
+		},
+	}
+	for _, test := range tests {
+		l := test.left
+		rcvd := l.Mag()
+		if rcvd != test.result {
+			t.Errorf("%v.Mag() != %v, got: %v", l, test.result, rcvd)
+
+		}
+	}
+}
+
+func TestDot(t *testing.T) {
+	tests := []struct {
+		left   SVector3
+		right  SVector3
+		result float32
+	}{
+		{
+			left:   SVector3{0, 0, 0},
+			right:  SVector3{0, 0, 0},
+			result: 0.0,
+		},
+		{
+			left:   SVector3{0, 1, 0},
+			right:  SVector3{0, 0, 1},
+			result: 0.0,
+		},
+		{
+			left:   SVector3{1, 0, 0},
+			right:  SVector3{0, 1, 0},
+			result: 0.0,
+		},
+	}
+	for _, test := range tests {
+		l := test.left
+		r := test.right
+		rcvd := l.Dot(r)
+		if rcvd != test.result {
+			t.Errorf("%v.Dot(%v) != %v, got: %v", l, r, test.result, rcvd)
+
+		}
+	}
+}
+
+func TestCross(t *testing.T) {
+	tests := []struct {
+		left   SVector3
+		right  SVector3
+		result SVector3
+	}{
+		{
+			left:   SVector3{0, 0, 0},
+			right:  SVector3{0, 0, 0},
+			result: SVector3{0, 0, 0},
+		},
+		{ // example take from: https://www.cuemath.com/geometry/cross-product/
+			left:   SVector3{3, 4, 5},
+			right:  SVector3{7, 8, 9},
+			result: SVector3{-4, 8, -4},
+		},
+		{ // example taken from:https://mathinsight.org/cross_product_examples
+			left:   SVector3{3, -3, 1},
+			right:  SVector3{4, 9, 2},
+			result: SVector3{-15, -2, 39},
+		},
+	}
+	for _, test := range tests {
+		l := test.left
+		r := test.right
+		rcvd := l.Cross(r)
+		if rcvd != test.result {
+			t.Errorf("%v.Cross(%v) != %v, got: %v", l, r, test.result, rcvd)
+
+		}
+	}
+}
+
+func TestNorm(t *testing.T) {
+	eqVecNorm := float32(0.57735026)
+	tests := []struct {
+		left   SVector3
+		result SVector3
+	}{
+		{
+			left:   SVector3{0, 0, 0},
+			result: SVector3{0, 0, 0},
+		},
+		{
+			left:   SVector3{1, 1, 1},
+			result: SVector3{eqVecNorm, eqVecNorm, eqVecNorm},
+		},
+		{
+			left:   SVector3{.1, .1, .1},
+			result: SVector3{eqVecNorm, eqVecNorm, eqVecNorm},
+		},
+		{
+			left:   SVector3{10, 10, 10},
+			result: SVector3{eqVecNorm, eqVecNorm, eqVecNorm},
+		},
+		{
+			left:   SVector3{10, 0, 0},
+			result: SVector3{1, 0, 0},
+		},
+		{
+			left:   SVector3{0, 10, 0},
+			result: SVector3{0, 1, 0},
+		},
+		{
+			left:   SVector3{0, 0, 10},
+			result: SVector3{0, 0, 1},
+		},
+	}
+	for _, test := range tests {
+		l := test.left
+		rcvd := l.Norm()
+		if rcvd != test.result && !rcvd.Almost(test.result) {
+			t.Errorf("%v.Norm() != %v, got: %v", l, test.result, rcvd)
+
+		}
+	}
+}

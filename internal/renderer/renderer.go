483531d0d7951955e4baef1d60c0af595b988fc5
Mark Smith <smith.h.mark@gmail.com>
2025-11-16 03:01:03 -0700

    added diffuse illumination

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -9,19 +9,91 @@ import (
 	"github.com/smithhmark/gotracer/internal/vector"
 )
 
+func background() color.Color {
+	return color.RGBA{R: 255, G: 255, B: 255, A: 0xff} // white
+}
+
+type Illumination struct {
+	Ambient     float64
+	Possitional []LightSource
+}
+
+func (i Illumination) ComputeLight(p, n vector.SVector3) float64 {
+	atPoint := i.Ambient
+
+	for _, lightSource := range i.Possitional {
+		atPoint += lightSource.ContributeLight(p, n)
+	}
+	return atPoint
+}
+
+type PointLight struct {
+	Location  vector.SVector3
+	Intensity float64
+}
+
+func lightHelper(l, p, n vector.SVector3, i float64) float64 {
+	dot := n.Dot(l)
+	if dot > 0 {
+		return i * dot / (n.Mag() * l.Mag())
+	}
+	return 0
+}
+
+func (pl PointLight) ContributeLight(p, n vector.SVector3) float64 {
+	l := pl.Location.Sub(p)
+	return lightHelper(l, p, n, pl.Intensity)
+}
+
+type DirectionalLight struct {
+	Direction vector.SVector3
+	Intensity float64
+}
+
+func (dl DirectionalLight) ContributeLight(p, n vector.SVector3) float64 {
+	l := dl.Direction.Sub(p)
+	return lightHelper(l, p, n, dl.Intensity)
+}
+
+type LightSource interface {
+	ContributeLight(p, n vector.SVector3) float64
+}
+
 type Scene struct {
+	Lighting Illumination
 	Objects  []Intersectable
 	Bg       color.Color
 	Observer vector.SVector3
 }
 
+func testLighting() Illumination {
+	var ill Illumination
+	ill.Ambient = 0.2
+	ill.Possitional = append(ill.Possitional, PointLight{Location: vector.SVector3{2, 1, 0}, Intensity: 0.6})
+	ill.Possitional = append(ill.Possitional, DirectionalLight{Direction: vector.SVector3{1, 4, 4}, Intensity: 0.2})
+	return ill
+}
+
 func testSpheres() Scene {
 	var sc Scene
+	sc.Lighting = Illumination{Ambient: 1.0}
+
 	sc.Bg = background()
 	sc.Objects = append(sc.Objects, Sphere{center: vector.SVector3{0, -1, 3}, radius: 1, shade: color.RGBA{255, 0, 0, 0xff}})
 	sc.Objects = append(sc.Objects, Sphere{center: vector.SVector3{2, 0, 4}, radius: 1, shade: color.RGBA{0, 0, 255, 0xff}})
 	sc.Objects = append(sc.Objects, Sphere{center: vector.SVector3{-2, 0, 4}, radius: 1, shade: color.RGBA{0, 255, 0, 0xff}})
 
+	return sc
+}
+
+func testSpheres2() Scene {
+	sc := testSpheres()
+	sc.Lighting = testLighting()
+	sc.Objects = append(sc.Objects,
+		Sphere{
+			center: vector.SVector3{0, -5001, 0},
+			radius: 5000,
+			shade:  color.RGBA{255, 255, 0, 0xff}})
 	return sc
 }
 
@@ -31,12 +103,16 @@ type Sphere struct {
 	shade  color.Color
 }
 
-func background() color.Color {
-	return color.RGBA{R: 255, G: 255, B: 255, A: 0xff} // white
-}
-
 func (s Sphere) Shade() color.Color {
 	return s.shade
+}
+
+func (s Sphere) Center() vector.SVector3 {
+	return s.center
+}
+
+func (s Sphere) Normal(o, d vector.SVector3) vector.SVector3 {
+	return vector.SVector3{}
 }
 
 func (s Sphere) Intersect(o, d vector.SVector3) (float64, float64) {
@@ -59,10 +135,12 @@ func (s Sphere) Intersect(o, d vector.SVector3) (float64, float64) {
 type Intersectable interface {
 	Intersect(o, d vector.SVector3) (float64, float64)
 	Shade() color.Color
+	Center() vector.SVector3
+	Normal(o, d vector.SVector3) vector.SVector3
 }
 
 func Render() image.Image {
-	sc := testSpheres()
+	sc := testSpheres2()
 	vp := vector.SVector3{1, 1, 1} // viewport TODO:move into Scene
 	rec := image.Rect(0, 0, 256, 256)
 	canvas := image.NewRGBA(rec)
@@ -77,6 +155,15 @@ func Render() image.Image {
 	}
 
 	return canvas
+}
+
+func scaleColor(shade color.Color, intensity float64) color.Color {
+	r, g, b, _ := shade.RGBA()
+	return color.RGBA{
+		R: uint8(intensity * float64(r)),
+		G: uint8(intensity * float64(g)),
+		B: uint8(intensity * float64(b)),
+		A: 0xff}
 }
 
 func traceRay(scene Scene, d vector.SVector3, near, far float64) color.Color {
@@ -97,7 +184,13 @@ func traceRay(scene Scene, d vector.SVector3, near, far float64) color.Color {
 	if closest_i < 0 {
 		return scene.Bg
 	}
-	return scene.Objects[closest_i].Shade()
+
+	obj := scene.Objects[closest_i]
+	p := scene.Observer.Add(d.Scale(closest_dist))
+	n := p.Sub(obj.Center()).Norm()
+	shade := obj.Shade()
+	lightLevel := scene.Lighting.ComputeLight(p, n)
+	return scaleColor(shade, lightLevel)
 	//return color.RGBA{R: 0, G: 0, B: 200, A: 0xff}
 }
 
